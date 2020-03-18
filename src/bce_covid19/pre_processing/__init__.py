cf340fa73e691e497b7e47f9ba0197605f13faeb
IoannisVardaxis <ioannis@oncoimmunity.com>
2020-03-18 12:43:24 +0100

    docs

--- src/bce_covid19/pre_processing/__init__.py
+++ src/bce_covid19/pre_processing/__init__.py
@@ -7,5 +7,8 @@ Details:
 =============
 For a given file (fasta) it will process the sequences in order to fit the parameters of the BCE predictor.
 
+Scripts:
+---------
+preproc.py:  main script for pre-processing
 
 """
