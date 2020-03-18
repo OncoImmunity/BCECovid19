53b5d021187a57fb8acaa30b47a140f730b6f2a1
IoannisVardaxis <ioannis@oncoimmunity.com>
2020-03-18 12:57:56 +0100

    docs

--- src/bce_covid19/__init__.py
+++ src/bce_covid19/__init__.py
@@ -13,12 +13,11 @@ pre_processing: package for pre-processing sequences before using the predictor.
 
 post_processing: package for post-processing predicting result.
 
+utils: utilities package for common functions
+
 Data:
 -----
 data: contains pre- and post-processing data
 
-Scripts:
---------
-paths.py: script for paths definition
 
 """
