26ee54606c3b6d1aea5794cdb4c273987963e2bf
IoannisVardaxis <ioannis@oncoimmunity.com>
2020-03-18 12:43:33 +0100

    adding paths for the data

--- src/bce_covid19/paths.py
+++ src/bce_covid19/paths.py
@@ -15,3 +15,17 @@ def get_data_path() -> str:
     """
     return os.path.join(os.path.dirname(__file__), "data")
 
+
+def get_ncbi_entrez_covid_19_path() -> str:
+    """
+    Get path of the ncbi_entrez_covid_19 folder
+
+    Returns: path of the directory of the `ncbi_entrez_covid_19` folder as string.
+
+    Example:
+        >>> path = get_ncbi_entrez_covid_19_path()
+        >>> print(path) # doctest: +ELLIPSIS
+        /.../BCECovid19/src/bce_covid19/data/ncbi_entrez_covid_19
+    """
+    return os.path.join(get_data_path(), "ncbi_entrez_covid_19")
+
