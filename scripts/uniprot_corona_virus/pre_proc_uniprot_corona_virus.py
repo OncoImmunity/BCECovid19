8410de90ce1f67c258ec1e00fe91b48ea10a54f0
IoannisVardaxis <ioannis@oncoimmunity.com>
2020-03-18 18:50:26 +0100

    adding data for uniprot

---
+++ scripts/uniprot_corona_virus/pre_proc_uniprot_corona_virus.py
@@ -0,0 +1,36 @@
+"""
+Pre-processing for UNIPROT CORONA VIRUS
+
+It will use the fasta file uniprot_coronavirus.fasta and process it in order to be able to run in the predictor.
+
+This means some filtering and possibly junking the the file into smaller ones.
+
+All of the files need to be run the predictor one by one.
+"""
+import logging
+import os
+
+from bce_covid19.pre_processing.preproc import pre_proc
+from bce_covid19.utils.logging import create_logging_file
+from bce_covid19 import paths
+
+if __name__ == '__main__':
+    # ----------
+    # create logger file:
+    # ----------
+    logger_file_name = "uniprot_corona_virus_pre_proc"
+    create_logging_file(logger_file_name=logger_file_name, save_path=os.path.dirname(__file__))
+    logger = logging.getLogger(__name__)
+    logger.info("=================================================")
+    logger.info("===== Pre-processing uniprot_corona_virus =======")
+    logger.info("=================================================")
+    # ----------
+    # run the pre-processing:
+    # ----------
+    tot_parsed_seq, tot_seq_filtered, junk_id = pre_proc(data_path=paths.get_uniprot_coronavirus_19_path(),
+                                                         file_name="uniprot_coronavirus.fasta",
+                                                         junk_name="uniprot_coronavirus")
+    logger.info(f"Total parsed sequences: {tot_parsed_seq}")
+    logger.info(f"Total filtered sequences: {tot_seq_filtered}")
+    logger.info(f"Total junks created: {junk_id}")
+    logger.info("Done")
